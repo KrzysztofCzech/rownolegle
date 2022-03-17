6321425604de8b03c82e25c47e47eddb2a666f32
Krzysztof <kczech123kczech123@gmail.com>
2022-03-17 13:58:46 +0100

    part 4

--- mpi_send.c
+++ mpi_send.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <mpi.h>
+#include <time.h>
 
 int main(int argc, char** argv) {
   MPI_Init(NULL, NULL);
@@ -17,9 +18,16 @@ int main(int argc, char** argv) {
     // If we are rank 0, set the number to -1 and send it to process 1
     number = -1;
     MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    printf("Process 0 send number %d to process 1\n", number);
+    MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    printf("Process 0 received number %d from process 1\n", number);
   } else if (world_rank == 1) {
     MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     printf("Process 1 received number %d from process 0\n", number);
+    number += 10;
+    MPI_Send(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    printf("Process 1 send number %d to process 0\n", number);
+
   }
   MPI_Finalize();
 }
