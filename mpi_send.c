183308b61ecd8f93bb7cf8b43be9fe5be0ff99d7
Krzysztof <kczech123kczech123@gmail.com>
2022-03-17 13:46:53 +0100

    part 3

--- mpi_send.c
+++ mpi_send.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include <mpi.h>
 
-int main (int argc, char * argv[])
-{
-    int rank, size;
-    MPI_Init (&argc, &argv);  /* starts MPI */
+int main(int argc, char** argv) {
+  MPI_Init(NULL, NULL);
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    // Find out rank, size
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-    int number = 10;
-    if (world_rank == 0) {
-        MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-    } else if (world_rank == 1) {
-        MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,
-                MPI_STATUS_IGNORE);
-        printf("Process 1 received number %d from process 0\n",
-            number);
-    }
-    MPI_Finalize();
-    return 0;
+  if (world_size < 2) {
+    printf("World size is smaller than 1 for %s\n", argv[0]);
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+  int number;
+  if (world_rank == 0) {
+    // If we are rank 0, set the number to -1 and send it to process 1
+    number = -1;
+    MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+  } else if (world_rank == 1) {
+    MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    printf("Process 1 received number %d from process 0\n", number);
+  }
+  MPI_Finalize();
 }
