e088e671c445f6fa8bd69dce326fc2436e601789
Krzysztof <kczech123kczech123@gmail.com>
2022-03-19 23:56:06 +0100

    log added

--- pole_kola.py
+++ pole_kola.py
@@ -40,3 +40,11 @@ if (comm.rank == 0):
     pi = 4.0*buff[0]/npoints
     print("Pi wynosi " + str(pi))
     print("czas = " + str(time2 - time1))
+
+if(comm.rank == 0):
+    with open('results_kolo.txt', 'a') as f:
+        f.write(f"PI = {pi}")
+        f.write('\n')
+        f.write(f"time = {time2 - time1}")
+        f.write('\n')
+        f.write(f"Size = {npoints}")
