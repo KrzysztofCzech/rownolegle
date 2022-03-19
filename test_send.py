148debea966678171c518d67ccf5ffcc7d750a14
Krzysztof <kczech123kczech123@gmail.com>
2022-03-19 10:04:01 +0100

    source fix

--- test_send.py
+++ test_send.py
@@ -16,7 +16,7 @@ buff = bytearray(256)
 MPI.Attach_buffer(buff)
 if mode == 1:
     if (comm.rank == 0):
-        comm.bsend(data, dest = 0, tag = 11)
+        comm.bsend(data, dest = 1, tag = 11)
         MPI.Detach_buffer()
     if (comm.rank == 1):
         data = comm.recv( source = 0, tag = 11)
@@ -25,9 +25,11 @@ time2 = MPI.Wtime()
 
 if mode == 0:
     if (comm.rank == 0):
-        comm.Ssend(data, dest = 0, tag = 11)
+        req = comm.isend(data, dest = 1, tag = 11)
+        req.wait()
     if (comm.rank == 1):
-        data = comm.recv( source = 0, tag = 11)
+        req = comm.irecv( source = 0, tag = 11)
+        data = req.wait()
         print("Data received mode 0")
 
 
