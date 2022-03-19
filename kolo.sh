c142ec78fcd8efd669a89851e0cc67f5c4e1335a
Krzysztof <kczech123kczech123@gmail.com>
2022-03-20 00:06:14 +0100

    sh file

---
+++ kolo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+#SBATCH --nodes 1
+#SBATCH --ntasks 4
+#SBATCH --time=00:10:00
+#SBATCH --partition=plgrid
+#SBATCH --account=plgmpr22
+
+module add plgrid/tools/openmpi
+
+mpiexec -np 4 ./pole_kola.py 10000
