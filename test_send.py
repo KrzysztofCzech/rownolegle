ce388b0ce8859b3e9d4c208d6bc319aa6014f48b
Krzysztof <kczech123kczech123@gmail.com>
2022-03-19 09:52:17 +0100

    send test

--- test_send.py
+++ test_send.py
@@ -12,13 +12,21 @@ if comm.size < 2:
 data = 0
 comm.barrier()
 time1 = MPI.Wtime()
+buff = bytearray(64)
+MPI.Attach_buffer(buff)
 if mode == 1:
     if (comm.rank == 0):
         comm.bsend(data, dest = 0, tag = 11)
-        print("works")
-        # data = comm.recv( source = 1, tag = 12)
-#     if (comm.rank == 1):
-#         data = comm.recv( source = 0, tag = 11)
-#         comm.send(data, dest = 0, tag = 12)
-# time2 = MPI.Wtime()
+    if (comm.rank == 1):
+        data = comm.recv( source = 0, tag = 11)
+        print("Data received mode 1")
+time2 = MPI.Wtime()
+
+if mode == 0:
+    if (comm.rank == 0):
+        comm.Ssend(data, dest = 0, tag = 11)
+    if (comm.rank == 1):
+        data = comm.recv( source = 0, tag = 11)
+        print("Data received mode 0")
+
 
