32e1b6c028f81f7ffe2cd205ba60986a7f21fc26
Krzysztof <kczech123kczech123@gmail.com>
2022-03-21 15:09:39 +0100

    time fix

--- kolo1.sh
+++ kolo1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
 #SBATCH --nodes 1
 #SBATCH --ntasks 12
-#SBATCH --time=00:10:00
+#SBATCH --time=00::00
 #SBATCH --partition=plgrid-short
 #SBATCH --account=plgmpr22
 
 module add plgrid/tools/openmpi
 module load plgrid/libs/python-mpi4py/3.0.1-python-3.6
 module add plgrid/libs/python-mpi4py/3.0.0-python-2.7
-mpiexec -np 1 ./pole_kola.py 100000000
+
 mpiexec -np 2 ./pole_kola.py 100000000
 mpiexec -np 3 ./pole_kola.py 100000000
 mpiexec -np 4 ./pole_kola.py 100000000
