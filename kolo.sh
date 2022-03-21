7459049e2999e3b7adc8a27ff21aadfcb80f2d75
Krzysztof <kczech123kczech123@gmail.com>
2022-03-21 15:04:04 +0100

    sh script

--- kolo.sh
+++ kolo.sh
@@ -6,5 +6,17 @@
 #SBATCH --account=plgmpr22
 
 module add plgrid/tools/openmpi
-module add plgrid/libs/python-mpi4py/3.0.1-python-3.6
-mpiexec -np 4 ./pole_kola.py 10000
+module load plgrid/libs/python-mpi4py/3.0.1-python-3.6
+module add plgrid/libs/python-mpi4py/3.0.0-python-2.7
+mpiexec -np 1 ./pole_kola.py 100000
+mpiexec -np 2 ./pole_kola.py 100000
+mpiexec -np 3 ./pole_kola.py 100000
+mpiexec -np 4 ./pole_kola.py 100000
+mpiexec -np 5 ./pole_kola.py 100000
+mpiexec -np 6 ./pole_kola.py 100000
+mpiexec -np 7 ./pole_kola.py 100000
+mpiexec -np 8 ./pole_kola.py 100000
+mpiexec -np 9 ./pole_kola.py 100000
+mpiexec -np 10 ./pole_kola.py 100000
+mpiexec -np 11 ./pole_kola.py 100000
+mpiexec -np 12 ./pole_kola.py 100000
