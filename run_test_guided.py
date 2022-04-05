27e669b77836d89f485a4098fa5426baec5cc233
Krzysztof <kczech123kczech123@gmail.com>
2022-04-05 13:15:54 +0200

    test files

---
+++ run_test_guided.py
@@ -0,0 +1,7 @@
+from subprocess import call 
+
+for i in range(0,10):
+    call(["./omp_guided", "1500000000", "1", "1000"])
+    call(["./omp_guided", "1500000000", "2", "1000"])
+    call(["./omp_guided", "1500000000", "3", "1000"])
+    call(["./omp_guided", "1500000000", "4", "1000"])
