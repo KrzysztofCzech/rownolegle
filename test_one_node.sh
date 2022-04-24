5042b768d8e6f72c2d3611abeca025f172efa7e4
Krzysztof <kczech123kczech123@gmail.com>
2022-04-24 12:31:50 +0200

    next

--- test_one_node.sh
+++ test_one_node.sh
@@ -1,23 +1,22 @@
 #!/bin/bash -l
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
-#SBATCH --time=00:50:00
+#SBATCH --time=00:05:00
 #SBATCH --partition=plgrid-short
 #SBATCH --account=plgmpr22
 
 
-./omp_sort 100000000 1 1
-./omp_sort 100000000 1 10
-./omp_sort 100000000 1 100
-./omp_sort 100000000 1 1000
-./omp_sort 100000000 1 10000
-./omp_sort 100000000 1 100000
-./omp_sort 100000000 1 1000000
-./omp_sort 100000000 1 2000000
-./omp_sort 100000000 1 5000000
-./omp_sort 100000000 1 8000000
-./omp_sort 100000000 1 10000000
-./omp_sort 100000000 1 20000000
+
+./omp_sort 1000000 1 1
+./omp_sort 1000000 1 10
+./omp_sort 1000000 1 100
+./omp_sort 1000000 1 1000
+./omp_sort 1000000 1 10000
+./omp_sort 1000000 1 20000
+./omp_sort 1000000 1 50000
+./omp_sort 1000000 1 80000
+./omp_sort 1000000 1 100000
+./omp_sort 1000000 1 200000
 
 
 
