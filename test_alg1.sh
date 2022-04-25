204c3ce5edb04b3eec71699d086e0b529800c8ba
Krzysztof <kczech123kczech123@gmail.com>
2022-04-26 00:07:05 +0200

    test sbatch updated

--- test_alg1.sh
+++ test_alg1.sh
@@ -1,19 +1,21 @@
 #!/bin/bash -l
 #SBATCH --nodes 1
 #SBATCH --ntasks 8
-#SBATCH --time=00:50:00
+#SBATCH --time=00:59:00
 #SBATCH --partition=plgrid-short
 #SBATCH --account=plgmpr22
 
-
-./omp_sort 1000000000 1 80000000
-./omp_sort 1000000000 2 40000000
-./omp_sort 1000000000 3 26666600
-./omp_sort 1000000000 4 20000000
-./omp_sort 1000000000 5 16000000
-./omp_sort 1000000000 6 13333300
-./omp_sort 1000000000 7 11428500
-./omp_sort 1000000000 8 10000000
+ for i in {0..1}
+    do
+    ./omp_sort3 1000000000 1 50000000
+    ./omp_sort3 1000000000 2 50000000
+    ./omp_sort3 1000000000 3 50000000
+    ./omp_sort3 1000000000 4 50000000
+    ./omp_sort3 1000000000 5 50000000
+    ./omp_sort3 1000000000 6 50000000
+    ./omp_sort3 1000000000 7 50000000
+    ./omp_sort3 1000000000 8 50000000
+done
 
 
 
