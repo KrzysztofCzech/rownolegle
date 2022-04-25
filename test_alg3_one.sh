76d0f19111219cc459ee827b8b879e6f220e1a26
Krzysztof <kczech123kczech123@gmail.com>
2022-04-25 23:33:42 +0200

    alg 3

---
+++ test_alg3_one.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --time=00:05:00
+#SBATCH --partition=plgrid-short
+#SBATCH --account=plgmpr22
+
+
+ for i in {0..4}
+    do
+    ./omp_sort3 100000000 1 1
+    ./omp_sort3 100000000 1 10
+    ./omp_sort3 100000000 1 100
+    ./omp_sort3 100000000 1 1000
+    ./omp_sort3 100000000 1 10000
+    ./omp_sort3 100000000 1 100000
+    ./omp_sort3 100000000 1 1000000
+    ./omp_sort3 100000000 1 10000000
+    ./omp_sort3 100000000 1 20000000
+    ./omp_sort3 100000000 1 50000000
+    ./omp_sort3 100000000 1 80000000
+    ./omp_sort3 100000000 1 10000000
+done
+
+
+
